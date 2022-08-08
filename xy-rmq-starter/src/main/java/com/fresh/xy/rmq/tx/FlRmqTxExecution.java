6d82c262150b300756f757e4d7674dd979c1923e
freshlml <1326191992@qq.com>
2022-08-08 18:54:41 +0800

    1

--- xy-rmq-starter/src/main/java/com/fresh/xy/rmq/tx/FlRmqTxExecution.java
+++ xy-rmq-starter/src/main/java/com/fresh/xy/rmq/tx/FlRmqTxExecution.java
@@ -13,13 +13,13 @@ public class FlRmqTxExecution<T extends RmqTxModel> {
     private T model;
 
     public FlRmqTxExecution(Object obj, T model, String methodName, Class<?>... paramTypes) {
-        AssertUtils.ifNull(obj, () -> "参数obj不能为空", null);
+        AssertUtils.notNull(obj, () -> "参数obj不能为空", null);
         AssertUtils.ifTrue(methodName==null || methodName.trim().length()==0, () -> "参数methodName不能为空", null);
-        AssertUtils.ifNull(model, () -> "参数model不能为空", null);
+        AssertUtils.notNull(model, () -> "参数model不能为空", null);
         this.obj = obj;
         this.model = model;
         Method methodExists = ReflectUtils.getMethod(obj.getClass(), methodName, paramTypes);
-        AssertUtils.ifNull(methodExists, () -> "methodName {" + methodName + "} not exists", null);
+        AssertUtils.notNull(methodExists, () -> "methodName {" + methodName + "} not exists", null);
         this.method = methodExists;
     }
 
