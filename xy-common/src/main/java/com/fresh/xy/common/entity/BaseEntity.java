9319e0c8979c7fef6174d6c223a5180d7a78e9ad
freshlml <1326191992@qq.com>
2023-03-30 11:05:06 +0800

    1

--- xy-common/src/main/java/com/fresh/xy/common/entity/BaseEntity.java
+++ xy-common/src/main/java/com/fresh/xy/common/entity/BaseEntity.java
@@ -25,13 +25,13 @@ public abstract class BaseEntity<ID> implements Serializable {
 	private ID id;
 
 	/** mysql-connector驱动包中默认使用SimpleDateFormat[yyyy-MM-dd][yyyy-MM-dd HH:mm:ss]将日期转化成String */
-	//@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")  //json序列化与反序列化的格式化器
-	//@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")  //spring中日期格式化器
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")  //json序列化与反序列化的格式化器
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")  //spring中日期格式化器
 	//private Date createTime;
 	private LocalDateTime createTime;
 
-	//@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
-	//@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	//private Date modifyTime;
 	private LocalDateTime modifyTime;
 
