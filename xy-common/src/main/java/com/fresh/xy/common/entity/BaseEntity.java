ca364dce9ca630f7aa8530894865a04c20a1559d
freshlml <1326191992@qq.com>
2023-04-01 20:15:58 +0800

    1

--- xy-common/src/main/java/com/fresh/xy/common/entity/BaseEntity.java
+++ xy-common/src/main/java/com/fresh/xy/common/entity/BaseEntity.java
@@ -2,8 +2,6 @@ package com.fresh.xy.common.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
